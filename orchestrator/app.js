d072c6bd1c18e2a89a3fcaa89760b6d32aa61fe3
fadhlanfm <fafadhlanriz@gmail.com>
2020-06-22 08:01:27 +0700

    orchestrator base

---
+++ orchestrator/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const routes = require('./routers')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/', routes)
+
+app.listen(PORT, () => console.log('listening to main server on', PORT))
